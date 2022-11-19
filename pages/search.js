b984804175a36a302b8762ed997fe8da1771ce05
Arnaud Steiner <arnaudsteiner@orange.fr>
2022-11-19 17:37:54 +0100

    google api search data call added with mockup of data received

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-export default function search() {
+export default function search({results}) {
 	return (
 		<>
 			<Head>
@@ -13,4 +14,22 @@ export default function search() {
 			<SearchHeader />
 		</>
 	);
+}
+
+//? back end call to google api
+export async function getServerSideProps(context) {
+	//? we are limited to 100 requests in a day so we use the mock data in development
+	const mockData = true;
+
+	const data = mockData ? Response : await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+	).then(
+		(response) => response.json()
+	);
+
+	return {
+		props: {
+			results: data
+		}
+	}
 }
